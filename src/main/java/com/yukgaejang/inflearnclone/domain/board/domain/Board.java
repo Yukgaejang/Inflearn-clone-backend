a0c1ebe342c1a0d3235041d94f4077fd9534ec5c
hyoseon <kimhyoseon928@gmail.com>
2024-06-30 03:38:17 +0900

    feat: Board 엔티티 생성 및 업데이터 메서드 추가

--- src/main/java/com/yukgaejang/inflearnclone/domain/board/domain/Board.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/board/domain/Board.java
@@ -2,16 +2,19 @@ package com.yukgaejang.inflearnclone.domain.board.domain;
 
 import com.yukgaejang.inflearnclone.domain.model.BaseEntity;
 import com.yukgaejang.inflearnclone.domain.user.domain.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 생성자를 protected로 유지
 public class Board extends BaseEntity {
 
     @Column(nullable = false, length = 255)
@@ -33,12 +36,47 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "tag_post",
+            joinColumns = @JoinColumn(name = "board_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
+
     @Builder
-    public Board(String title, String content, String category, User user) {
+    public Board(String title, String content, String category, User user, Set<Tag> tags) {
         this.title = title;
         this.content = content;
         this.category = category;
         this.user = user;
+        this.tags = tags;
+        this.likeCount = 0L;
+        this.viewCount = 0L;
+        this.updatedAt = null;
     }
 
+    // Post 생성
+    public void createPost(User user, String title, String content, String category, Set<Tag> tags) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.tags = tags;
+        this.likeCount = 0L;
+        this.viewCount = 0L;
+        this.updatedAt = null;
+    }
+
+    // Post 수정
+    public void updatePost(String title, String content, String category, Set<Tag> tags) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.tags = tags;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
