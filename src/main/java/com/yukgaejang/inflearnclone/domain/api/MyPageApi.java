b65a851f55a0b33301ccb7484ba282f13d35070d
Jung ui heon <ky7662@naver.com>
2024-07-01 22:23:48 +0900

    refactor : 파일 다시 생성

---
+++ src/main/java/com/yukgaejang/inflearnclone/domain/api/MyPageApi.java
@@ -0,0 +1,18 @@
+package com.yukgaejang.inflearnclone.domain.api;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mypage")
+public class MyPageApi {
+
+    @GetMapping
+    public String getMyPage() {
+        return "mypage";
+    }
+}
