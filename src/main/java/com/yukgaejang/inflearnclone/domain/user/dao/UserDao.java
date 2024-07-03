3dd6be3b8349c3d38dd0f0af3c6b33b8a1d5545f
hyoseon <kimhyoseon928@gmail.com>
2024-07-03 13:54:46 +0900

    Test: 사용자 데이터 삽입용 코드

--- src/main/java/com/yukgaejang/inflearnclone/domain/user/dao/UserDao.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/user/dao/UserDao.java
@@ -1,4 +1,4 @@
-package com.yukgaejang.inflearnclone.domain.board.dao;
+package com.yukgaejang.inflearnclone.domain.user.dao;
 
 import com.yukgaejang.inflearnclone.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
