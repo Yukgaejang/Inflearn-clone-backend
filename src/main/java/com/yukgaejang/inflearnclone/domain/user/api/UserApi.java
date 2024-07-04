7e7153a4bb2ca35aa1ed454c0df1ba9eea259670
Jung ui heon <ky7662@naver.com>
2024-07-04 15:24:24 +0900

    refactor : 충돌 리팩토링

--- src/main/java/com/yukgaejang/inflearnclone/domain/user/api/UserApi.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/user/api/UserApi.java
@@ -1,5 +1,6 @@
 package com.yukgaejang.inflearnclone.domain.user.api;
 
+import com.yukgaejang.inflearnclone.domain.user.application.UserService;
 import com.yukgaejang.inflearnclone.domain.user.domain.User;
 import com.yukgaejang.inflearnclone.domain.user.dto.CreateUserDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,4 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserApi {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    @Operation(summary = "Create a new user", description = "API to create a new user")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+        User user = userService.createUser(createUserDto);
+        return ResponseEntity.ok(user);
+    }
 }
