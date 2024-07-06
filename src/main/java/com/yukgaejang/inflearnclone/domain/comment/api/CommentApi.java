8cf2960eeb2bc3944555758adc9fe88c981f9aa1
wooyong99 <wulovesk@naver.com>
2024-07-06 19:59:36 +0900

    feat: Comment API 별 권한부여

--- src/main/java/com/yukgaejang/inflearnclone/domain/comment/api/CommentApi.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/comment/api/CommentApi.java
@@ -6,7 +6,9 @@ import com.yukgaejang.inflearnclone.domain.comment.application.CommentService;
 import com.yukgaejang.inflearnclone.domain.comment.dto.BoardCreateRequest;
 import com.yukgaejang.inflearnclone.domain.comment.dto.BoardUpdateRequest;
 import com.yukgaejang.inflearnclone.domain.comment.dto.CommentFindAllResponse;
+import com.yukgaejang.inflearnclone.domain.login.util.SecurityUtil;
 import com.yukgaejang.inflearnclone.global.common.response.ApiResponse;
+import com.yukgaejang.inflearnclone.global.error.NotFoundUserException;
 import io.swagger.v3.oas.annotations.Operation;
 import java.security.Principal;
 import java.util.List;
@@ -38,7 +40,10 @@ public class CommentApi {
     @Operation(summary = "게시글에 대한 댓글 등록", description = "content는 필수 값입니다.")
     public ResponseEntity<ApiResponse<Void>> save(@RequestBody BoardCreateRequest request,
         @PathVariable Long boardId, Principal principal) {
-        commentService.save(request, Long.parseLong(principal.getName()), boardId);
+        if (SecurityUtil.getCurrentUsername().get().equals("anonymousUser")) {
+            throw new NotFoundUserException();
+        }
+        commentService.save(request, principal.getName(), boardId);
         return ResponseEntity.ok().build();
     }
 
@@ -47,15 +52,21 @@ public class CommentApi {
     public ResponseEntity<ApiResponse<Void>> update(
         @RequestBody BoardUpdateRequest request, @PathVariable Long boardId,
         @PathVariable Long commentId, Principal principal) {
-        commentService.update(request, boardId, commentId, Long.parseLong(principal.getName()));
+        if (SecurityUtil.getCurrentUsername().get().equals("anonymousUser")) {
+            throw new NotFoundUserException();
+        }
+        commentService.update(request, boardId, commentId, principal.getName());
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/board/{boardId}/comments/{commentId}")
+    @DeleteMapping("/boards/{boardId}/comments/{commentId}")
     @Operation(summary = "게시글에 종속된 특정 댓글 삭제", description = "bordId와 commentId는 필수 값입니다.")
     public ResponseEntity<ApiResponse<Void>> remove(
         @PathVariable Long boardId, @PathVariable Long commentId, Principal principal) {
-        commentService.delete(boardId, commentId, Long.parseLong(principal.getName()));
+        if (SecurityUtil.getCurrentUsername().get().equals("anonymousUser")) {
+            throw new NotFoundUserException();
+        }
+        commentService.delete(boardId, commentId, principal.getName());
         return ResponseEntity.ok().build();
     }
 
