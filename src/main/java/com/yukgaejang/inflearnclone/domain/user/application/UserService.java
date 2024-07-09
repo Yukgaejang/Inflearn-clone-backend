c0cd1d559712ae8e138c2c59e09d65ac2615e3c3
Jung ui heon <ky7662@naver.com>
2024-07-09 16:24:40 +0900

    마이페이지 service 구현

--- src/main/java/com/yukgaejang/inflearnclone/domain/user/application/UserService.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/user/application/UserService.java
@@ -1,8 +1,12 @@
 package com.yukgaejang.inflearnclone.domain.user.application;
 
 import com.yukgaejang.inflearnclone.domain.user.dao.UserDao;
+import com.yukgaejang.inflearnclone.domain.user.dto.UserBoardResponseDto;
+import com.yukgaejang.inflearnclone.domain.user.dto.UserCommentResponseDto;
 import com.yukgaejang.inflearnclone.domain.user.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +15,14 @@ public class UserService {
     private final UserDao userRepository;
 
     public UserResponseDto getUser(String email) {
-        UserResponseDto userResponseDto = userRepository.getUserWithPostAndCommentCounts(email);
-        return userResponseDto;
+        return userRepository.getUserWithPostAndCommentCounts(email);
     }
 
+    public Page<UserBoardResponseDto> getUserBoard(String email, Pageable pageable, String sortBy) {
+        return userRepository.getUserBoardData(email, pageable, sortBy);
+    }
 
+    public Page<UserCommentResponseDto> getUserComment(String email, Pageable pageable, String sortBy) {
+        return userRepository.getUserCommentData(email, pageable, sortBy);
+    }
 }
