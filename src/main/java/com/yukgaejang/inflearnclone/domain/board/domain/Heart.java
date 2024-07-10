bec5731d836e148d644f08cb95adb4b6d10ea59c
hyoseon <kimhyoseon928@gmail.com>
2024-07-10 15:34:53 +0900

    feat: top post 기능을 위한 좋아요 관련 데이터

--- src/main/java/com/yukgaejang/inflearnclone/domain/board/domain/Heart.java
+++ src/main/java/com/yukgaejang/inflearnclone/domain/board/domain/Heart.java
@@ -1,11 +1,19 @@
 package com.yukgaejang.inflearnclone.domain.board.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.yukgaejang.inflearnclone.domain.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -25,9 +33,23 @@ public class Heart {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @CreatedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
     @Builder
     public Heart(Board board, User user) {
         this.board = board;
         this.user = user;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
     }
 }
