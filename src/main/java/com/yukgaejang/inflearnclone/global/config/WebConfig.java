fb57b16043dc81b39aee90e3ae20b15a4003bc3e
hyoseon <kimhyoseon928@gmail.com>
2024-07-11 13:44:27 +0900

    feat: cors 코드 변경

--- src/main/java/com/yukgaejang/inflearnclone/global/config/WebConfig.java
+++ src/main/java/com/yukgaejang/inflearnclone/global/config/WebConfig.java
@@ -1,28 +1,8 @@
 package com.yukgaejang.inflearnclone.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-//@Configuration
-//public class WebConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("*")
-////                        .allowedOrigins("http://localhost:8080","http://localhost:9090", "https://wooyong.shop", "https://www.wooyong.shop")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-//}
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -30,13 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("http://localhost:8080", "http://localhost:9090","http://localhost:3000", "https://wooyong.shop", "https://www.wooyong.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 }
-
-
